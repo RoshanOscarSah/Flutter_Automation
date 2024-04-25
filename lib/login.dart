2cf28a57c3427040be25b9009590c2a78cca2800
RoshanWebandApp <roshan@webandapp.com.np>
2024-04-25 15:45:30 +0545

    test for login  todo

---
+++ lib/login.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Login',
+            ),
+            //INPUT EMAIL
+            //INPUT PASSWORD
+            // BUTTON LOGIN
+          ],
+        ),
+      ),
+    );
+  }
+}
